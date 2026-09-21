c34a85d24b77e33411838b3c742a2c43ca21ec38
ahabwe blessing <ahabwegird1@gmail.com>
2026-09-21 19:21:47 +0300

    Service portal

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
+
+Route::get('/',[HomeController::class, 'index'])->name('index');
+Route::get('/contact',[HometController::class, 'contact'])>('contact');
+Route::post('/store', [ContactController::class, 'store'])->name('contact.store');
+
+
+
+
